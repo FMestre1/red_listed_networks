afaf02b49dbe6c52ce5306790522153355609042
FMestre1 <mestre.frederico@gmail.com>
2023-01-04 21:24:04 +0000

    04jan2023

---
+++ red_listed.R
@@ -0,0 +1,15 @@
+#FMestre
+#04-01-2023
+
+
+load("iberian_fw_new_GLOBI_comm_collection_IGRAPH.RData")
+load("iberian_fw_MAIORANO_comm_collection_IGRAPH.RData")
+load("iberian_fw_new_comm_collection_new_version_28SET2022_IGRAPH.RData")
+#
+load("iberian_fw_new_comm_collection_new_version_28SET2022_ADJACENCY_MATRICES.RData")
+load("iberian_fw_MAIORANO_comm_collection_IGRAPH_ADJACENCY_MATRICES.RData")
+load("iberian_fw_new_GLOBI_comm_collection_IGRAPH_ADJACENCY_MATRICES.RData")
+#
+load("iberian_fw_new_comm_collection_new_version_28SET2022.RData")
+load("iberian_fw_new_GLOBI_comm_collection.RData")
+load("iberian_fw_MAIORANO_comm_collection.RData")
