e61672736da63d4fd167b16d49dbbd0b70785d9d
FMestre1 <mestre.frederico@gmail.com>
2023-01-05 10:57:16 +0000

    05jan2023

--- red_listed.R
+++ red_listed.R
@@ -1,6 +1,7 @@
 #FMestre
 #04-01-2023
 
+library(rredlist) #API - xxx
 
 load("iberian_fw_new_GLOBI_comm_collection_IGRAPH.RData")
 load("iberian_fw_MAIORANO_comm_collection_IGRAPH.RData")
@@ -13,3 +14,12 @@ load("iberian_fw_new_GLOBI_comm_collection_IGRAPH_ADJACENCY_MATRICES.RData")
 load("iberian_fw_new_comm_collection_new_version_28SET2022.RData")
 load("iberian_fw_new_GLOBI_comm_collection.RData")
 load("iberian_fw_MAIORANO_comm_collection.RData")
+
+#Which species?
+species <- lapply(iberian_fw_new_comm_collection_new_version_28SET2022_ADJACENCY_MATRICES, colnames)
+species <- unique(unlist(species))
+
+rl_version()
+
+rl_sp_country(country = "Portugal",  key = NULL)
+
