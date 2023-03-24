2dca142dca37a664e5eb95804fbf269f7afb8d3f
Frederico Mestre <34461749+FMestre1@users.noreply.github.com>
2023-03-24 17:05:56 +0000

    gathering BS data

--- 2.nuria_data.R
+++ 2.nuria_data.R
@@ -38,3 +38,37 @@ length(species_names2)
 save(species_names2, file = "species_names2.RData")
 
 #fw_list[[1]]
+
+
+################################################################################
+#                   SECOND NÚRIA DATASET (network metrics)
+################################################################################
+
+
+dataset2_nuria <- list.files("C:\\Users\\FMest\\Documents\\0. Artigos\\IUCN_networks\\data\\data_nuria_2")
+length(dataset2_nuria)
+
+metrics_dataset_2 <- data.frame()
+
+for(i in 1:length(dataset2_nuria)){
+  
+  m123 <- read.csv(paste0("C:\\Users\\FMest\\Documents\\0. Artigos\\IUCN_networks\\data\\data_nuria_2\\", dataset2_nuria[i]))
+  grid_code <- strsplit(dataset2_nuria[i], ".csv")[[1]]
+  metrics_dataset_2 <- rbind(metrics_dataset_2, data.frame(grid_code,m123))
+  #
+  message(paste0("Did ", i, "!"))
+  
+}
+
+View(metrics_dataset_2)
+
+#nrow(metrics_dataset_2)
+#View(metrics_dataset_2)
+
+metrics_dataset_3 <- unique(metrics_dataset_2)
+#View(metrics_dataset_3)
+#nrow(metrics_dataset_3)
+
+#Save
+save(metrics_dataset_3, file = "metrics_dataset_FINAL.RData")
+
