e4a3efebcaee70fe2ed4b546d18b4708c31334a5
FMestre1 <mestre.frederico@gmail.com>
2024-10-11 13:19:53 +0200

    overlap computation

--- 10.comparing_maps_new.R
+++ 10.comparing_maps_new.R
@@ -804,19 +804,32 @@ library(terra)
 
 min_vals_ivi <- pmin(values(ivi_nt_spatial_raster_lower_res), values(ivi_t_spatial_raster_lower_res), na.rm = TRUE)
 min_vals_centrality <- pmin(values(centrality_nt_spatial_raster_lower_res), values(centrality_t_spatial_raster_lower_res), na.rm = TRUE)
-
 min_vals_outdegree <- pmin(values(outdegree_nt_spatial_raster_lower_res), values(outdegree_t_spatial_raster_lower_res), na.rm = TRUE)
 min_vals_indegree <- pmin(values(indegree_nt_spatial_raster_lower_res), values(indegree_t_spatial_raster_lower_res), na.rm = TRUE)
 min_vals_closeness <- pmin(values(closeness_nt_spatial_raster_lower_res), values(closeness_t_spatial_raster_lower_res), na.rm = TRUE)
 min_vals_tl <- pmin(values(tl_nt_spatial_raster_lower_res), values(tl_t_spatial_raster_lower_res), na.rm = TRUE)
 
+#Save
+saveRDS(min_vals_ivi, "min_vals_ivi.rds")
+saveRDS(min_vals_centrality, "min_vals_centrality.rds")
+saveRDS(min_vals_outdegree, "min_vals_outdegree.rds")
+saveRDS(min_vals_indegree, "min_vals_indegree.rds")
+saveRDS(min_vals_closeness, "min_vals_closeness.rds")
+saveRDS(min_vals_tl, "min_vals_tl.rds")
+
+#Load
+min_vals_ivi <- readRD("min_vals_ivi.rds")
+min_vals_centrality <- readRDS("min_vals_centrality.rds")
+min_vals_outdegree <- readRDS("min_vals_outdegree.rds")
+min_vals_indegree <- readRDS("min_vals_indegree.rds")
+min_vals_closeness <- readRDS("min_vals_closeness.rds")
+min_vals_tl <-readRDS("min_vals_tl.rds")
 
 # Calculate the Overlap Coefficient
 ivi_overlap_coefficient <- 2 * sum(min_vals_ivi, na.rm = TRUE) / (sum(values(ivi_nt_spatial_raster_lower_res), na.rm = TRUE) + sum(values(ivi_t_spatial_raster_lower_res), na.rm = TRUE))
 #ivi_overlap_coefficient
 centrality_overlap_coefficient <- 2 * sum(min_vals_centrality, na.rm = TRUE) / (sum(values(centrality_nt_spatial_raster_lower_res), na.rm = TRUE) + sum(values(centrality_t_spatial_raster_lower_res), na.rm = TRUE))
 #centrality_overlap_coefficient
-
 outdegree_overlap_coefficient <- 2 * sum(min_vals_outdegree, na.rm = TRUE) / (sum(values(outdegree_nt_spatial_raster_lower_res), na.rm = TRUE) + sum(values(outdegree_t_spatial_raster_lower_res), na.rm = TRUE))
 #outdegree_overlap_coefficient
 indegree_overlap_coefficient <- 2 * sum(min_vals_indegree, na.rm = TRUE) / (sum(values(indegree_nt_spatial_raster_lower_res), na.rm = TRUE) + sum(values(indegree_t_spatial_raster_lower_res), na.rm = TRUE))
@@ -829,7 +842,6 @@ tl_overlap_coefficient <- 2 * sum(min_vals_tl, na.rm = TRUE) / (sum(values(tl_nt
 #Save
 saveRDS(ivi_overlap_coefficient, "ivi_overlap_coefficient.rds")
 saveRDS(centrality_overlap_coefficient, "centrality_overlap_coefficient.rds")
-
 saveRDS(outdegree_overlap_coefficient, "outdegree_overlap_coefficient.rds")
 saveRDS(indegree_overlap_coefficient, "indegree_overlap_coefficient.rds")
 saveRDS(closeness_overlap_coefficient, "closeness_overlap_coefficient.rds")
@@ -839,7 +851,6 @@ saveRDS(tl_overlap_coefficient, "tl_overlap_coefficient.rds")
 ivi_overlap_coefficient <- readRDS("~/github/red_listed_networks/ivi_overlap_coefficient.rds")
 centralty_overlap_coefficient <- readRDS("~/github/red_listed_networks/centralty_overlap_coefficient.rds")
 outdegree_overlap_coefficient <- readRDS("~/github/red_listed_networks/outdegree_overlap_coefficient.rds")
-
 indegree_overlap_coefficient <- readRDS("~/github/red_listed_networks/indegree_overlap_coefficient.rds")
 closeness_overlap_coefficient <- readRDS("~/github/red_listed_networks/closeness_overlap_coefficient.rds")
 tl_overlap_coefficient <- readRDS("~/github/red_listed_networks/tl_overlap_coefficient.rds")
